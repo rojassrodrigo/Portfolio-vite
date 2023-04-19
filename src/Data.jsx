89844d823a98ef3146992f5a1ca830e8edce1296
rojassrodrigo <rojasrodrigo@live.com>
2023-04-19 18:09:23 -0300

    portfolio 26

--- src/Data.jsx
+++ src/Data.jsx
@@ -1,6 +1,6 @@
 import caratula1 from "../src/images/petopia.png";
 import appviajes from "../src/images/chatbot5.jpg";
-import appdogs from "../src/images/appsdog.png";
+import appdogs from "../src/images/appstore.png";
 import appecom from "../src/images/appecom.png";
 import appecom2 from "../src/images/appecom2.png"
 import appecom3 from "../src/images/appecom3.png"
@@ -14,6 +14,14 @@ export const projects = [
       link: "https://fe-shelter.vercel.app/",
     },
     {
+      title: "Apple Store",
+      subtitle: "Javascript, React and Redux",
+      description: "Este es un prototipo de ecommerce de venta de iPhone que se encuentra en desarrollo fue hecho con JS, React, Redux y Bootstrap. Además, cuenta con un sistema de login de Google, una pasarela de pagos con Paypal y un carrito de compras intuitivo para una experiencia de usuario fluida y satisfactoria.",
+      image: appdogs,
+      link: "https://aplicacion-comercio-electronico.vercel.app/",
+    },
+    
+    {
       title: "Chatbot con IA",
       subtitle: "TypeScript, React and Tailwind",
       description:
@@ -21,37 +29,30 @@ export const projects = [
       image: appviajes,
       link: "https://chatbot-phi-ten.vercel.app/",
     },
-    {
-      title: "DevChat",
-      subtitle: "React and Firebase",
-      description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-      image: appdogs,
-      link: "https://jsbootcamp.com",
-    },
-    {
-      title: "Epic Todo App",
-      subtitle: "React Hooks",
-      description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-      image: appecom,
-      link: "https://pythonbootcamp.com",
-    },    {
-      title: "Epic Todo App",
-      subtitle: "React Hooks",
-      description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-      image: appecom3,
-      link: "https://pythonbootcamp.com",
-    },
-    {
-      title: "Epic Todo App",
-      subtitle: "React Hooks",
-      description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-      image: appecom2,
-      link: "https://pythonbootcamp.com",
-    },
+    
+    // {
+    //   title: "Epic Todo App",
+    //   subtitle: "React Hooks",
+    //   description:
+    //     "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    //   image: appecom,
+    //   link: "https://pythonbootcamp.com",
+    // },    {
+    //   title: "Epic Todo App",
+    //   subtitle: "React Hooks",
+    //   description:
+    //     "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    //   image: appecom3,
+    //   link: "https://pythonbootcamp.com",
+    // },
+    // {
+    //   title: "Epic Todo App",
+    //   subtitle: "React Hooks",
+    //   description:
+    //     "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    //   image: appecom2,
+    //   link: "https://pythonbootcamp.com",
+    // },
 
   ];
 
