4f16b0ca7d2b86c89a4e9f8cfeaa3d5e7dd2b50c
rojassrodrigo <rojasrodrigo@live.com>
2023-05-03 19:30:46 -0300

    36 commit

--- src/componentes/About.jsx
+++ src/componentes/About.jsx
@@ -1,36 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import css from '../css/About.module.css';
 import perfil from '../images/perfil2.jpg';
 import cv from '../images/Rodrigo_Rojas.pdf'
 import { TypeAnimation } from 'react-type-animation';
 
 export default function About() {
+
   return (
     <section id="about">
       <div className={css.card}>
         <div className={css.cardContent}>
-          <TypeAnimation
-            sequence={[
-              'Rodrigo Victor Rojas', // Types 'Hola, soy Rodrigo'
-              1000, // Waits 1s
-              'Full Stack Developer',
-              1000,
-              () => {
-                console.log('Sequence completed');
-              },
-            ]}
-            speed={145}
-            style={{
-              fontFamily: 'Roboto, sans-serif',
-              fontSize: '2.7rem',
-              marginBottom: '10px',
-              lineHeight: '1.5',
-              fontWeight: 'bold',
-              display: 'block',
-              textShadow: '2px 2px #529',
-            }}
-            repeat={Infinity}
-          />
+        {window.innerWidth > 700 ? (
+            <TypeAnimation
+              sequence={['Rodrigo Victor Rojas',1000,'Full Stack Developer',1000,() => {console.log('Sequence completed');},]}
+              speed={145}
+              style={{
+                fontFamily: 'Roboto, sans-serif',
+                fontSize: '2.7rem',
+                marginBottom: '10px',
+                lineHeight: '1.5',
+                fontWeight: 'bold',
+                display: 'block',
+                textShadow: '2px 2px #529',
+              }}
+              repeat={Infinity}
+            />
+          ) : (
+           <h1>Rodrigo Victor Rojas </h1>
+        )}
+
           <p>
           Soy un profesional con conocimientos en Backend y Frontend, 
           lo que me ha permitido destacar en la resolución de conflictos y la organización.
